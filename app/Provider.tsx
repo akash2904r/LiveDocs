e86632f0584c2de25b0961710bb0201891d69282
Akash <akash2904r@gmail.com>
2024-12-28 22:29:09 +0530

    Add liveblocks provider and config

---
+++ app/Provider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Loader from "@/components/Loader";
+import { ClientSideSuspense, LiveblocksProvider } from "@liveblocks/react/suspense";
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+    return (
+        <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+            <ClientSideSuspense fallback={<Loader />}>
+                {children}
+            </ClientSideSuspense>
+        </LiveblocksProvider>
+    );
+}
