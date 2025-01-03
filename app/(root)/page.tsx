7211490e531a8db72346796e7e42ea5c54f2ff2f
Akash <akash2904r@gmail.com>
2025-01-03 21:39:23 +0530

    Basic setup for Home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,48 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
+import Header from "@/components/Header";
+import AddDocumentBtn from "@/components/AddDocumentBtn";
+
+export default async function Home() {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect('/sign-in');
+
+  const documents = [];
+
   return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+
+      {documents.length > 0 ? (
+        <div>
+
+        </div>
+      ): (
+        <div className="document-list-empty">
+          <Image 
+            src="/assets/icons/doc.svg"
+            alt="Document"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+
+          <AddDocumentBtn 
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+          />
+        </div>
+      )}
+    </main>
   );
 }
