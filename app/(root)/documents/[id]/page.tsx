3e51cd56f64b43438f6aea01871d56022d1b2fb3
Akash <akash2904r@gmail.com>
2025-01-03 21:35:35 +0530

    Add Collaborative Room component to Document

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,7 +1,26 @@
-export default function Document() {
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs/server";
+
+import { getDocument } from "@/lib/actions/room.actions";
+import CollaborativeRoom from "@/components/CollaborativeRoom";
+
+export default async function Document({ params: { id }}: SearchParamProps) {
+    const clerkUser = await currentUser();
+    if (!clerkUser) redirect('/sign-in');
+
+    const room = await getDocument({ 
+        roomId: id, 
+        userId: clerkUser.emailAddresses[0].emailAddress,
+    });
+
+    if (!room) redirect('/');
+
     return (
-        <div>
-            
-        </div>
+        <main className="flex w-full flex-col items-center">
+            <CollaborativeRoom 
+                roomId={id}
+                roomMetadata={room.metadata}
+            />
+        </main>
     );
 }
