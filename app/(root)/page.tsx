8eaf9fd60767bba80a3337ecee0399c57614d7da
Akash <akash2904r@gmail.com>
2025-01-11 21:58:56 +0530

    Add show created documents functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 import { SignedIn, UserButton } from "@clerk/nextjs";
@@ -5,12 +6,14 @@ import { currentUser } from "@clerk/nextjs/server";
 
 import Header from "@/components/Header";
 import AddDocumentBtn from "@/components/AddDocumentBtn";
+import { getDocuments } from "@/lib/actions/room.actions";
+import { dateConverter } from "@/lib/utils";
 
 export default async function Home() {
   const clerkUser = await currentUser();
   if (!clerkUser) redirect('/sign-in');
 
-  const documents = [];
+  const roomDocuments = await getDocuments(clerkUser.emailAddresses[0].emailAddress);
 
   return (
     <main className="home-container">
@@ -23,9 +26,35 @@ export default async function Home() {
         </div>
       </Header>
 
-      {documents.length > 0 ? (
-        <div>
-
+      {roomDocuments.data.length > 0 ? (
+        <div className="document-list-container">
+          <div className="document-list-title">
+            <h3 className="text-28-semibold">All Documents</h3>
+            <AddDocumentBtn
+              userId={clerkUser.id}
+              email={clerkUser.emailAddresses[0].emailAddress}
+            />
+          </div>
+          <ul className="document-ul">
+            {roomDocuments.data.map(({ id, metadata, createdAt }: any) => (
+              <li key={id} className="document-list-item">
+                <Link href={`/documents/${id}`} className="flex flex-1 items-center gap-4">
+                  <div className="hidden rounded-md bg-dark-500 p-2 sm:block">
+                    <Image
+                      src="/assets/icons/doc.svg"
+                      alt="file"
+                      width={40}
+                      height={40}
+                    />
+                  </div>
+                  <div className="space-y-3">
+                    <p className="line-clamp-1 text-lg">{metadata.title}</p>
+                    <p className="text-sm font-light text-blue-100">Created about {dateConverter(createdAt)}</p>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
       ): (
         <div className="document-list-empty">
