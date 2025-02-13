0cb14eeec53f3e9b976e383fb1a123c93b529ba5
Akash <akash2904r@gmail.com>
2025-02-13 21:10:22 +0530

    Add get document users functinality

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,6 +2,7 @@
 
 import { clerkClient } from "@clerk/nextjs/server";
 import { parseStringify } from "@/lib/utils";
+import { liveblocks } from "../liveblocks";
 
 export const getClerkUsers = async ({ userIds }: { userIds: string[] }) =>  {
     try {
@@ -22,5 +23,31 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[] }) =>  {
         return parseStringify(sortedUsers);
     } catch (error) {
         console.log(error);
+    }
+}
+
+export const getDocumentUsers = async ({ 
+    roomId, 
+    currentUser, 
+    text 
+}: { 
+    roomId: string, 
+    currentUser: string, 
+    text: string 
+}) => {
+    try {
+        const room = await liveblocks.getRoom(roomId);
+
+        const users = Object.keys(room.usersAccesses).filter((email) => email !== currentUser);
+
+        if (text.length) {
+            const lowerCaseText = text.toLowerCase();
+
+            const filteredUsers = users.filter((email: string) => email.toLowerCase().includes(lowerCaseText));
+
+            return parseStringify(filteredUsers);
+        }
+    } catch (error) {
+        console.log(`Error fetching document users: ${error}`);
     }
 }
